ed595ea34a5a953e5e04799dbc61476111ddfdd5
DemoUser77 <katykatykaty001@gmail.com>
2023-07-25 00:09:45 +0300

    first update

--- routes/api/contacts-routes.js
+++ routes/api/contacts-routes.js
@@ -3,18 +3,21 @@ import express from "express";
 import contactsSchemas from "../../schemas/contacts-schemas.js";
 import { validateBody } from "../../decorators/index.js";
 import {contactsController} from "../../controllers/index.js";
-import { isEmptyBody } from "../../middlewares/index.js";
+import { isEmptyBody,isValidId, isEmptyBodyFavorite } from "../../middlewares/index.js";
 
 const contactsRouter = express.Router();
 
 contactsRouter.get('/', contactsController.getAll);
 
-contactsRouter.get('/:id', contactsController.getById);
+contactsRouter.get('/:id', isValidId, contactsController.getById);
 
 contactsRouter.post('/', isEmptyBody, validateBody(contactsSchemas.contactAddSchema), contactsController.add);
 
-contactsRouter.delete('/:id', contactsController.deleteById);
+contactsRouter.delete('/:id', isValidId, contactsController.deleteById);
 
-contactsRouter.put('/:id', isEmptyBody, validateBody(contactsSchemas.contactAddSchema), contactsController.updateById);
+contactsRouter.put('/:id',isValidId, isEmptyBody, validateBody(contactsSchemas.contactAddSchema), contactsController.updateById);
+
+contactsRouter.patch("/:id/favorite", isValidId, isEmptyBodyFavorite, validateBody(contactsSchemas.contactUpdateFavoriteSchema), contactsController.updateStatusContact)
+
 
 export default contactsRouter;
